1877c0899448e5f34e4648e27bb78b82a3dce60c
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-06-13 05:55:59 +0000

    feat(util): add OSM and Apple Maps link builders as alternatives

--- src/main/java/dev/geolocate/util/GoogleMapsLink.java
+++ src/main/java/dev/geolocate/util/GoogleMapsLink.java
@@ -4,30 +4,54 @@ import dev.geolocate.mapping.GeoPoint;
 
 public final class GoogleMapsLink {
 
-    private static final String BASE_URL = "https://www.google.com/maps?q=";
-    private static final String DETAILED_URL = "https://www.google.com/maps/@%s,%s,%dz";
+    private static final String GOOGLE_MAPS_URL = "https://www.google.com/maps/@%s,%s,%dz";
+    private static final String GOOGLE_MAPS_SIMPLE = "https://www.google.com/maps?q=%s,%s";
+    private static final String GOOGLE_MAPS_LABEL = "https://www.google.com/maps?q=%s,%s(%s)";
+    private static final String GOOGLE_MAPS_DIRECTIONS = "https://www.google.com/maps/dir/%s,%s/%s,%s";
+    private static final String OSM_URL = "https://www.openstreetmap.org/?mlat=%s&mlon=%s&zoom=%d";
+    private static final String APPLE_MAPS_URL = "https://maps.apple.com/?ll=%s,%s&z=%d";
 
     private GoogleMapsLink() {}
 
     public static String build(GeoPoint point, int zoom) {
-        String lat = String.valueOf(point.getLatitude());
-        String lon = String.valueOf(point.getLongitude());
-        return String.format(DETAILED_URL, lat, lon, zoom);
+        return String.format(GOOGLE_MAPS_URL,
+                point.getLatitude(),
+                point.getLongitude(),
+                zoom);
     }
 
     public static String buildSimple(GeoPoint point) {
-        return BASE_URL + point.getLatitude() + "," + point.getLongitude();
+        return String.format(GOOGLE_MAPS_SIMPLE,
+                point.getLatitude(),
+                point.getLongitude());
     }
 
     public static String buildWithLabel(GeoPoint point, String label) {
-        return BASE_URL + point.getLatitude() + "," + point.getLongitude()
-                + "(" + label.replace(" ", "+") + ")";
+        return String.format(GOOGLE_MAPS_LABEL,
+                point.getLatitude(),
+                point.getLongitude(),
+                label.replace(" ", "+"));
     }
 
     public static String buildDirections(GeoPoint from, GeoPoint to) {
-        return "https://www.google.com/maps/dir/"
-                + from.getLatitude() + "," + from.getLongitude()
-                + "/"
-                + to.getLatitude() + "," + to.getLongitude();
+        return String.format(GOOGLE_MAPS_DIRECTIONS,
+                from.getLatitude(),
+                from.getLongitude(),
+                to.getLatitude(),
+                to.getLongitude());
+    }
+
+    public static String buildOpenStreetMap(GeoPoint point, int zoom) {
+        return String.format(OSM_URL,
+                point.getLatitude(),
+                point.getLongitude(),
+                zoom);
+    }
+
+    public static String buildAppleMaps(GeoPoint point, int zoom) {
+        return String.format(APPLE_MAPS_URL,
+                point.getLatitude(),
+                point.getLongitude(),
+                zoom);
     }
 }
