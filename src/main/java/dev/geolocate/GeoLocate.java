c5443f0b8e5af418ed30f350e6515ce97a8661c0
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-06-13 03:54:52 +0000

    Implement GeoLocate plugin structure and functionality

--- src/main/java/dev/geolocate/GeoLocate.java
+++ src/main/java/dev/geolocate/GeoLocate.java
@@ -1,1 +1,72 @@
+package dev.geolocate;
 
+import dev.geolocate.api.GeoLocateAPI;
+import dev.geolocate.api.WorldMapper;
+import dev.geolocate.command.GeoLocateCommand;
+import dev.geolocate.config.GeoLocateConfig;
+import dev.geolocate.listener.PlayerMoveListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GeoLocate extends JavaPlugin {
+
+    private static GeoLocate instance;
+    private GeoLocateConfig geoConfig;
+    private WorldMapper worldMapper;
+    private GeoLocateAPI api;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+
+        this.geoConfig = new GeoLocateConfig(this);
+        this.worldMapper = new WorldMapper(this);
+        this.api = new GeoLocateAPI(this);
+
+        registerCommands();
+        registerListeners();
+
+        getLogger().info("GeoLocate v" + getDescription().getVersion() + " enabled.");
+        getLogger().info("Loaded " + worldMapper.getConfiguredWorldCount() + " world mapping(s).");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("GeoLocate disabled.");
+        instance = null;
+    }
+
+    private void registerCommands() {
+        GeoLocateCommand handler = new GeoLocateCommand(this);
+        getCommand("geolocate").setExecutor(handler);
+        getCommand("geolocate").setTabCompleter(handler);
+        getCommand("geoadmin").setExecutor(handler);
+        getCommand("geoadmin").setTabCompleter(handler);
+    }
+
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new PlayerMoveListener(this), this);
+    }
+
+    public void reload() {
+        reloadConfig();
+        this.geoConfig = new GeoLocateConfig(this);
+        this.worldMapper = new WorldMapper(this);
+    }
+
+    public static GeoLocate getInstance() {
+        return instance;
+    }
+
+    public GeoLocateConfig getGeoConfig() {
+        return geoConfig;
+    }
+
+    public WorldMapper getWorldMapper() {
+        return worldMapper;
+    }
+
+    public GeoLocateAPI getAPI() {
+        return api;
+    }
+}
