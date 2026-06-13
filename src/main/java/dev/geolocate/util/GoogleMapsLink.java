ddbe356d8d8823ccd31eaf4793bb818fa7366efa
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-06-13 03:57:33 +0000

    Update GoogleMapsLink.java

--- src/main/java/dev/geolocate/util/GoogleMapsLink.java
+++ src/main/java/dev/geolocate/util/GoogleMapsLink.java
@@ -1,1 +1,33 @@
+package dev.geolocate.util;
 
+import dev.geolocate.mapping.GeoPoint;
+
+public final class GoogleMapsLink {
+
+    private static final String BASE_URL = "https://www.google.com/maps?q=";
+    private static final String DETAILED_URL = "https://www.google.com/maps/@%s,%s,%dz";
+
+    private GoogleMapsLink() {}
+
+    public static String build(GeoPoint point, int zoom) {
+        String lat = String.valueOf(point.getLatitude());
+        String lon = String.valueOf(point.getLongitude());
+        return String.format(DETAILED_URL, lat, lon, zoom);
+    }
+
+    public static String buildSimple(GeoPoint point) {
+        return BASE_URL + point.getLatitude() + "," + point.getLongitude();
+    }
+
+    public static String buildWithLabel(GeoPoint point, String label) {
+        return BASE_URL + point.getLatitude() + "," + point.getLongitude()
+                + "(" + label.replace(" ", "+") + ")";
+    }
+
+    public static String buildDirections(GeoPoint from, GeoPoint to) {
+        return "https://www.google.com/maps/dir/"
+                + from.getLatitude() + "," + from.getLongitude()
+                + "/"
+                + to.getLatitude() + "," + to.getLongitude();
+    }
+}
