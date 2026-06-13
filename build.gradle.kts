5124c8f546d6bbdba621d70ecd3acb582afee8aa
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-06-13 03:53:47 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,60 @@
+import org.gradle.jvm.tasks.Jar
 
+plugins {
+    java
+    `maven-publish`
+    id("com.gradleup.shadow") version "8.3.5"
+}
+
+group = property("group")!!
+version = property("version")!!
+description = property("description")!!
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release.set(21)
+}
+
+tasks.processResources {
+    filesMatching("plugin.yml") {
+        expand(
+            "version" to project.version,
+            "description" to project.description
+        )
+    }
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+    minimize()
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = project.group.toString()
+            artifactId = "geolocate"
+            version = project.version.toString()
+        }
+    }
+}
